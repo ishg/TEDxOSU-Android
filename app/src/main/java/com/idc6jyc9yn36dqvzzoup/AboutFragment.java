3f4546a69fcd69a46867b229156e9d087bb47d0b
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2016-02-10 17:42:04 -0500

    Pages set up for tabs

---
+++ app/src/main/java/com/idc6jyc9yn36dqvzzoup/AboutFragment.java
@@ -0,0 +1,28 @@
+package com.idc6jyc9yn36dqvzzoup;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ishmeet on 2/10/16.
+ */
+public class AboutFragment extends Fragment {
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
