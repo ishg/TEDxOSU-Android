227f11287edf3932a1e89d078854e22bd3594810
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2016-02-22 16:45:31 -0500

    Social Buttons Work

--- app/src/main/java/com/idc6jyc9yn36dqvzzoup/HomeFragment.java
+++ app/src/main/java/com/idc6jyc9yn36dqvzzoup/HomeFragment.java
@@ -1,21 +1,24 @@
 package com.idc6jyc9yn36dqvzzoup;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-/**
- * Created by ishmeet on 2/10/16.
- */
 public class HomeFragment extends Fragment{
     private String headline = "Join TEDxOhioStateUniversity for our 5th annual event. This year's theme, Reconstructing Reality, will feature students, faculty, staff, and alumni as speakers and performers who will inspire and challenge our concepts of science, technology, history, and life.";
     private String locationLabel = "Mershon Auditorium";
     private String locationDesc = "1871 N High St, Columbus, OH";
     private String timeLabel = "Saturday, March 5th, 2016";
     private String timeDesc = "11 AM to 6 PM";
+    Context context;
+    Intent intent;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,6 +27,7 @@ public class HomeFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = getContext();
     }
 
     @Override
@@ -38,8 +42,60 @@ public class HomeFragment extends Fragment{
         ((TextView) rootView.findViewById(R.id.timeLabelText)).setText(timeLabel);
         ((TextView) rootView.findViewById(R.id.timeDescText)).setText(timeDesc);
 
+        rootView.findViewById(R.id.buttonWebsite).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://tedx.osu.edu"));
+                startActivity(intent);
 
+            }
+        });
+        rootView.findViewById(R.id.buttonFacebook).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+
+                try {
+                    context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
+                    intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("fb://facewebmodal/f?href=https://www.facebook.com/TEDxOhioStateU"));
+                } catch (Exception e) {
+                    intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/TEDxOhioStateU"));
+                }
+                startActivity(intent);
+            }
+        });
+        rootView.findViewById(R.id.buttonTwitter).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                try {
+                    context.getPackageManager().getPackageInfo("com.android.twitter", 0);
+                    intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?screen_name=TEDxOhioStateU"));
+                } catch (Exception e) {
+                    intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://twitter.com/TEDxOhioStateU"));
+                }
+                startActivity(intent);
+            }
+        });
+        rootView.findViewById(R.id.buttonInstagram).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                try {
+                    context.getPackageManager().getPackageInfo("com.android.instagram", 0);
+                    intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("instagram://user?username=tedxohiostateu"));
+                } catch (Exception e) {
+                    intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/tedxohiostateu"));
+                }
+                startActivity(intent);
+            }
+        });
+        rootView.findViewById(R.id.buttonYoutube).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=XSvhWs2M9JI&list=PLB18FCCC5AD700BFB"));
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
+
 }
