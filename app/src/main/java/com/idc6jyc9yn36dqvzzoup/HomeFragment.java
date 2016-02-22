b4d7f8ccec606b7a20161bc32cb16e4d46e6f07d
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2016-02-22 15:09:06 -0500

    Home Page Layout

--- app/src/main/java/com/idc6jyc9yn36dqvzzoup/HomeFragment.java
+++ app/src/main/java/com/idc6jyc9yn36dqvzzoup/HomeFragment.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by ishmeet on 2/10/16.
  */
 public class HomeFragment extends Fragment{
+    private String headline = "Join TEDxOhioStateUniversity for our 5th annual event. This year's theme, Reconstructing Reality, will feature students, faculty, staff, and alumni as speakers and performers who will inspire and challenge our concepts of science, technology, history, and life.";
+    private String locationLabel = "Mershon Auditorium";
+    private String locationDesc = "1871 N High St, Columbus, OH";
+    private String timeLabel = "Saturday, March 5th, 2016";
+    private String timeDesc = "11 AM to 6 PM";
+
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -23,6 +30,16 @@ public class HomeFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        ((TextView) rootView.findViewById(R.id.homeHeadlineText)).setText(headline);
+        ((TextView) rootView.findViewById(R.id.locationLabelText)).setText(locationLabel);
+        ((TextView) rootView.findViewById(R.id.locationDescText)).setText(locationDesc);
+        ((TextView) rootView.findViewById(R.id.timeLabelText)).setText(timeLabel);
+        ((TextView) rootView.findViewById(R.id.timeDescText)).setText(timeDesc);
+
+
+
+        return rootView;
     }
 }
